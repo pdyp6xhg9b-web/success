e4b791b97e3f0e91c78febb72993d8a8ead8295e
美花 <pdyp6xhg9b@privaterelay.appleid.com>
2026-07-04 23:33:46 +0800

    Add files via upload

---
+++ 自增，逻辑操作，字符串.c
@@ -0,0 +1,63 @@
+////////#include<stdio.h>
+////////#include<string.h>
+////////
+////////// /0是字符但是不算字符串内容   空格也算字符;
+////////
+////////
+////////int main()
+////////{
+////////    int num1=10;
+////////    int num2=20;
+////////    if(num1>num2)
+////////        printf("较大值是：%d\n",num1);
+////////    else
+////////        printf("较大值是：%d\n",num2);
+////////    return 0;
+////////}
+////////
+////////
+//////
+//////
+//////#include<stdio.h>
+//////#include<string.h>
+//////int main()
+//////{
+//////    int a=10;
+//////    int b=a++;//后置自增，先使用再自增,a=11,b=10；
+//////    printf("a=%d b=%d\n",a,b);
+//////    int c=10;
+//////    int d=++c;
+//////    printf("c=%d d=%d\n",c,d);//前置自增，先++再使用；
+//////    return 0;
+//////}
+////
+////
+////#include<stdio.h>
+////#include<string.h>
+////
+////int main()
+////{
+////    int a=(int)3.14;//double-int;
+////    printf("%d\n",a);
+////    return 0;
+////}
+//#include<stdio.h>
+//#include<string.h>
+//
+//int main()
+//{
+//    int a=3;
+//    int b=5;
+//    int c =a&&b;
+//    printf("%d\n",c);
+//   // &&//逻辑与，并且,真假；
+//    //  ||逻辑或；
+//    return 0;
+//}
+//
+#include<stdio.h>
+#include<string.h>
+
+
+    //[]下标引用，（）函数调用；
+
