5d73d2c77aa59f29b8ff72385bbf8eaf58560435
美花 <pdyp6xhg9b@privaterelay.appleid.com>
2026-07-11 23:08:35 +0800

    Add files via upload
    分支和循环（2）
    太晚了今天没好好学习，明天一定要早起写完，当码农的女孩不会输！哦呀斯密！

---
+++ 0711未完善.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    /*
+    int ch=0;
+    while((ch=getchar())!=EOF)//文件结束标志;
+    {
+        putchar(ch);
+    }
+     */
+    char password[20]={0};
+    printf("请输入密码:>");
+    scanf("%s",password);
+    
+    
+    
+    
+    
+    return 0;
+}
